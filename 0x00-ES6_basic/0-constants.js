698ae8f0297b3065485b513d9fa58c2b30986805
kwamboka1 <makorimildred@gmail.com>
2022-06-14 11:00:56 +0000

    [Fix]:The code

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -8,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  const combination = 'But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
