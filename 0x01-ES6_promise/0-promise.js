3887a4e4c5c303a49d3cc7ac4ba62c6308d29847
Mildred Makori <makorimildred@gmail.com>
2022-06-16 13:10:58 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const a = 1;
-
-    if (a === 1) {
-      resolve('Random number is one');
-    } else {
-      reject(Error('Not one'));
-    }
-  });
+  return new Promise((resolve, reject) => {});
 }
