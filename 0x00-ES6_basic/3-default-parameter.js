02cd8b72a692e07b3089795806a42633d57ae5fc
kwamboka1 <makorimildred@gmail.com>
2022-06-16 11:58:46 +0000

    [Fix]:Identation error

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,8 +1,7 @@
 /*eslint-desable*/
 export default function getSumOfHoods(
-
-initialNumber,
- expansion1989 = 89,
- expansion2019 = 19,) {
- return initialNumber + expansion1989 + expansion2019;
+	initialNumber,
+	expansion1989 = 89,
+	expansion2019 = 19,) {
+	return initialNumber + expansion1989 + expansion2019;
 }
