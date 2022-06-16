b0d933fb27d34efb9c694525c55cd23437f707da
Mildred Makori <makorimildred@gmail.com>
2022-06-16 14:45:20 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
+/*eslint-desable*/
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    //eslint-disable-next-line
     		const task = true;
 
     		const task2 = false;
