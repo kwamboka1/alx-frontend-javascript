133b0e85589832e9d97fb8599bbb44ed014d90c4
Mildred Makori <makorimildred@gmail.com>
2022-06-18 16:21:11 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,25 @@
+/* eslint-disable */
 export default class Building {
-  constructor(sqft = 0) {
-    if (this.constructor !== Building
-        && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+  constructor(sqft) {
+    if (
+      this.constructor !== Building &&
+      this.evacuationWarningMessage === undefined
+    ) {
+      throw new Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
     }
-
-    this._sqft = sqft;
+    this.sqft = sqft;
   }
+
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== "number") {
+      throw new TypeError("Sqft must be a number");
+    }
+    this._sqft = sqft;
   }
 }
