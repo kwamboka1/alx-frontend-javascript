36b48f1cec52f7a59a8f394a90677be170f13e16
kwamboka1 <makorimildred@gmail.com>
2022-06-21 21:41:20 +0000

    [Clean-set]: A function that returns a string of all the set values that start with a specific string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function cleanSet(set, startString) {
+  let result = "";
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
+    if (i && i.startsWith(startString))
+      result += `${i.slice(startString.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
+}
